af331c0040580318d6d47531f95d5192b5a7b81f
uzytkownik <manuladysweety7@gmail.com>
2023-11-25 18:24:34 +0100

    Dodanie dwóch zadań

---
+++ zad1.py
@@ -0,0 +1,32 @@
+"""
+Załóżmy, że pracujesz nad programem do
+analizy mediów społecznościowych. Podane są
+trzy poniższe zmienne:
+
+liczba_uzytkownikow = 9045
+
+liczba_postow = 54822
+
+liczba_polubien = 251561
+
+Oblicz średnią liczbę postów na użytkownika,
+dzieląc całkowitą liczbę postów przez liczbę
+użytkowników. Przypisz wynik do zmiennej o
+nazwie posty_na_uzytkownika.
+
+Oblicz średnią liczbę polubień na post, dzieląc
+całkowitą liczbę polubień przez liczbę postów.
+Przypisz wynik do zmiennej o nazwie
+polubienia_na_post.
+"""
+
+
+liczba_uzytkownikow = 9045
+liczba_postow = 54822
+liczba_polubien = 251561
+
+posty_na_uzytkownika = liczba_postow / liczba_uzytkownikow
+
+polubienia_na_post = liczba_polubien / liczba_postow
+
+
---
+++ zad2.py
@@ -0,0 +1,14 @@
+"""
+Do zmiennej cena przypisz wartość produktu
+równą 69.99 PLN oraz do zmiennej zniżka
+przypisz wartość 0.1 (zniżka 10%). Następnie
+policz cenę tego produktu po zastosowaniu
+podanej zniżki. Wynik wydrukuj do konsoli
+"""
+
+cena = 69.99
+znizka = 0.1
+
+cena_po_znizce = cena - cena*znizka
+
+print(cena_po_znizce)
